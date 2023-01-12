e01925a6ab6db17e30bab24a8c4c8446a502d8db
andrewkhristich <tab111andrew@gmail.com>
2023-01-13 02:43:34 +0800

    revert Dockerfiles

--- python-simple-action/build.sh
+++ python-simple-action/build.sh
@@ -3,13 +3,13 @@
 ROOT=$(dirname $0)
 cd $ROOT
 
-ACTION_NAME=ak-simple-test-action
-BUILD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-BRANCH_NAME_LOWER=`echo $BUILD_BRANCH | tr '[:upper:]' '[:lower:]'`
+ACTION_NAME=gate-python-action1
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-IMAGE=docker-hub.just-ai.com/caila-actions/$ACTION_NAME:sdktest-3
+IMAGE=docker-hub.just-ai.com/caila-actions/$ACTION_NAME:$BRANCH
+DOCKER_BUILDKIT=1 docker build . --ssh default -t $IMAGE
 
-eval $(ssh-agent)
-ssh-add $HOME/.ssh/id_rsa
-export DOCKER_BUILDKIT=1
-docker build . --ssh default -t $IMAGE
+echo $IMAGE
+
+#docker push $IMAGE
+
