fbad753d4f106c039ed9a5f2f18612075025dd43
oleg <gerbylev.oleg@gmail.com>
2023-10-06 21:15:00 +0300

    first

--- python-simple-action/build.sh
+++ python-simple-action/build.sh
@@ -7,9 +7,9 @@ BRANCH=$(echo $1 | tr '[:upper:]' '[:lower:]')
 ACTION_NAME=simple-action-example
 IMAGE=docker-hub.just-ai.com/caila-actions/$ACTION_NAME:$BRANCH
 
-./set_mlp_sdk_version.sh "$BRANCH"
+#./set_mlp_sdk_version.sh "$BRANCH"
 
-DOCKER_BUILDKIT=1 docker build . -t "$IMAGE"
+docker build . -t "$IMAGE"
 
 echo "$IMAGE"
 
